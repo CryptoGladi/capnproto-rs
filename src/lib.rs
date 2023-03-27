f748b420b2f1f11aa86dd9b095fe495985e8c251
CryptoGladi <mymamchm@mail.ru>
2023-03-27 20:40:31 +0300

    done byte order!

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,9 @@
-use byteorder::{ByteOrder, NetworkEndian, NativeEndian, WriteBytesExt, ReadBytesExt, BigEndian};
 use std::io::Cursor;
-
-pub enum Value {
-    Bool(bool),
-    String(String),
-}
-
-pub trait CapnprotoDeserialize {
-    fn deserialize(&self) -> Vec<u8>;
-}
-
-struct Lol {
-
-}
-
-impl CapnprotoDeserialize for Lol {
-    fn deserialize(&self) -> Vec<u8> {
-        let mut buf = vec![0; 4];
-        byteorder::NetworkEndian::write_i32(&mut buf, 1233333);
-        
-        buf
-    }
-}
 
 #[test]
 fn lolll() {
-    let raw_string = "test".as_bytes();
-    byteorder::NetworkEndian::write_i16(buf, n)
+    let mut raw_string = 124i32.to_be_bytes();
+    println!("network: {:?}", raw_string);
+
+    println!("normal: {:?}", i32::from_be_bytes(raw_string));
 }
